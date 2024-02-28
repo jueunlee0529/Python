2906d9c46d97623170ba38890baeb62de2d8d346
jueunlee0529 <108929037+jueunlee0529@users.noreply.github.com>
2024-02-28 18:41:30 +0900

    Add files via upload

---
+++ 연스.py
@@ -0,0 +1,11 @@
+n = int(input(" "))
+a = 0
+
+for i in range(1, n+1) :
+
+    if i % 3 == 0 :
+        a += i
+
+print(a)
+    
+    
---
+++ 연습 2.py
@@ -0,0 +1,8 @@
+a = 0
+
+for i in range (1, 11) :
+    n = int(input(" "))
+
+    if n > a :
+        a = n
+print(a)
